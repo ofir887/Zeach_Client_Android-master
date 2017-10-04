a48fd71db3907a2e041d68809d50243e66dbd7ab
ofir887 <ofir887@users.noreply.github.com>
2017-10-04 18:45:42 +0300

    add message handler & raycast algorithm to check if point inside polygon

---
+++ app/src/main/java/com/zeach/ofirmonis/zeach/Constants/GpsConstants.java
@@ -0,0 +1,12 @@
+package com.zeach.ofirmonis.zeach.Constants;
+
+/**
+ * Created by ofirmonis on 04/10/2017.
+ */
+
+public class GpsConstants {
+
+    public static final int GET_GPS_COORDINATES = 0;
+    public static final int GET_USER_COUNTRY = 1;
+
+}
