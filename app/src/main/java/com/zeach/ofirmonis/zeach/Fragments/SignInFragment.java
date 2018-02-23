2f611435ec54a54e7d1851f2732f1ec36038ade1
ofir887 <ofir.monis@gmail.com>
2018-02-23 22:22:14 +0200

    Add Alert Dialog on sign in

--- app/src/main/java/com/zeach/ofirmonis/zeach/Fragments/SignInFragment.java
+++ app/src/main/java/com/zeach/ofirmonis/zeach/Fragments/SignInFragment.java
@@ -1,5 +1,7 @@
 package com.zeach.ofirmonis.zeach.Fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -11,14 +13,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -38,7 +37,7 @@ import static com.facebook.FacebookSdk.getApplicationContext;
  * Created by ofirmonis on 31/05/2017.
  */
 
-public class SignInFragment extends Fragment implements View.OnClickListener{
+public class SignInFragment extends Fragment implements View.OnClickListener {
     private CallbackManager callbackManager;
     private DatabaseReference data;
     private FirebaseAuth mAuth;
@@ -48,17 +47,17 @@ public class SignInFragment extends Fragment implements View.OnClickListener{
     private TextView PasswordTextView;
 
     private com.zeach.ofirmonis.zeach.Objects.User User;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        this.rootView  =inflater.inflate(R.layout.signin_fragment,container,false);
+        this.rootView = inflater.inflate(R.layout.signin_fragment, container, false);
 
-        SignInButton = (Button)this.rootView.findViewById(R.id.signin_button);
-        EmailTextView = (TextView)this.rootView.findViewById(R.id.email_textfield);
-        PasswordTextView = (TextView)this.rootView.findViewById(R.id.password_textfield);
+        SignInButton = (Button) this.rootView.findViewById(R.id.signin_button);
+        EmailTextView = (TextView) this.rootView.findViewById(R.id.email_textfield);
+        PasswordTextView = (TextView) this.rootView.findViewById(R.id.password_textfield);
         this.data = FirebaseDatabase.getInstance().getReference();
         SignInButton.setOnClickListener(this);
-
 
 
         return rootView;
@@ -72,73 +71,64 @@ public class SignInFragment extends Fragment implements View.OnClickListener{
         mAuth = FirebaseAuth.getInstance(); //sign in
 
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode,resultCode,data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-    private void handleFacebookAccessToken(AccessToken token) {
 
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            User.setEmail(user.getEmail());
-                            User.setUID(user.getUid());
-                            User.setProvider(user.getProviderData().get(0).toString());
-                            SendUserAndMoveToProfileActivity();
-                        } else {
-
-                        }
-                    }
-                });
-    }
 
     @Override
     public void onClick(View v) {
-        if (v == SignInButton){
+        if (v == SignInButton) {
             String email = this.EmailTextView.getText().toString();
             String password = this.PasswordTextView.getText().toString();
-            createNewFirebaseUser(email,password);
-
-
+            createNewFirebaseUser(email, password);
         }
     }
-    public void SendUserAndMoveToProfileActivity(){
+
+    public void SendUserAndMoveToProfileFragment() {
         this.data = FirebaseDatabase.getInstance().getReference();
-      //  User.AddFriendToList("hgg","ofir");
-       // User.AddFriendToList("hggfdf","ofihr");
         Map<String, com.zeach.ofirmonis.zeach.Objects.User> user = new HashMap<String, com.zeach.ofirmonis.zeach.Objects.User>();
-       // user.put(this.User.getUID(),this.User);
         User.setProfilePictureUri("/PersonIcon.png");
         data.child(FirebaseConstants.USERS).child(this.User.getUID()).setValue(this.User);
         AppController.getInstance().setUser(this.User); // save user in singleton
-        //Add seperate parent ! need to check if this is good or can out this on Users in nested map
-        //data.child("Users").child(this.User.getUID()).child("Friends").push().child("ofir");
         Intent profileActivity = new Intent(getActivity(), MainActivity.class);
         profileActivity.putExtra("map", false);
         getActivity().finish();
         startActivity(profileActivity);
     }
 
-    public void createNewFirebaseUser(String email,String password){
+    public void createNewFirebaseUser(String email, String password) {
         this.mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        AlertDialog.Builder signInDialog = new AlertDialog.Builder(getContext());
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
                             User = new User(user.getEmail(), user.getUid(), user.getProviderId());
-                            SendUserAndMoveToProfileActivity();
+                            signInDialog.setTitle("SignIn Completed");
+                            signInDialog.setMessage("Success !");
+                            signInDialog.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                    SendUserAndMoveToProfileFragment();
+                                }
+                            });
                         } else {
-
+                            signInDialog.setTitle("SignIn Failed");
+                            signInDialog.setMessage("Failed. Try again !");
+                            signInDialog.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
                         }
-
-                        // ...
+                        signInDialog.show();
                     }
                 });
     }
