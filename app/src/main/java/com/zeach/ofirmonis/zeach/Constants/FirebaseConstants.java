222012e588d025669bbb705bca5b3d21826c7530
ofir887 <ofir.monis@gmail.com>
2018-02-24 22:16:43 +0200

    Add Constants

--- app/src/main/java/com/zeach/ofirmonis/zeach/Constants/FirebaseConstants.java
+++ app/src/main/java/com/zeach/ofirmonis/zeach/Constants/FirebaseConstants.java
@@ -10,19 +10,38 @@ public class FirebaseConstants {
     public static final String USERS = "Users";
     public static final String FRIENDS_LIST = "friendsList";
     public static final String BEACHES = "Beaches";
-    public static final String ID = "id";
+    public static final String ID = "uid";
     public static final String FACEBOOK_UID = "facebookUID";
-    public static final String FRIENDS = "friends";
-    public static final String TRAFFIC = "Traffic";
+    public static final String FRIENDS = "friendsList";
     public static final String FRIENDS_REQUESTS = "FriendsRequest";
     public static final String AWAITING_CONFIRMATION = "AwaitingConfirmation";
     public static final String FAVORITE_BEACHES = "favoriteBeaches";
+    public static final String CURRENT_BEACH = "current_beach";
+    public static final String EMAIL = "email";
+    public static final String PRIVATE_PROFILE = "privateProfile";
+    public static final String PROVIDER = "provider";
+    public static final String IMPORT_FACEBBOK_FRIENDS = "import_facebook_friends";
+    public static final String NAME = "name";
+    public static final String PHOTO_URL = "photoUrl";
 
     //Beach
+    public static final String TRAFFIC = "Traffic";
+    public static final String BEACH_ID = "BeachID";
+    public static final String BEACH_LISTENER_ID = "BeachListenerID";
+    public static final String COUNTRY = "Country";
+    public static final String CURRENT_DEVICES = "CurrentDevices";
+    public static final String PEOPLE_LIST = "Peoplelist";
+    public static final String COORDS = "Coords";
 
-
-    //Beach Listener
+    //Coords
+    public static final String LATITUDE = "lat";
+    public static final String LONGITUDE = "lng";
 
     //TimeStamp Constants
     public static final String TIMESTAMPS = "Timestamps";
+
+    //Feedback
+    public static final String ACCURATE = "Accurate";
+    public static final String EASY_TO_USE = "EasyToUse";
+    public static final String RATING = "Rating";
 }
